a2df758ed192f0adcb9c99008cf076e7ae8cb268
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2022-11-13 12:39:55 -0300

    desafio produto

--- curso_1/curso-graphql-aluno/schema_queries/projeto/index.js
+++ curso_1/curso-graphql-aluno/schema_queries/projeto/index.js
@@ -3,6 +3,13 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
     scalar Date
 
+    type Produto{
+        nome:String!
+        preco:Float!
+        desconto:Float       
+        precoComDesconto:Float
+    }
+
     type Usuario{
         id: ID!
         nome:String!
@@ -18,10 +25,20 @@ const typeDefs = gql`
         ola:String
         horaAtual:Date!
         usuarioLogado: Usuario
+        produtoEmDestaque:Produto
     }
 `
 
 const resolvers = {
+    Produto:{
+        precoComDesconto(produto){
+            if(produto.desconto){
+                return produto.preco * (1 - produto.desconto)       
+            }else{
+                return produto.preco
+            }
+        }
+    },
     //resolve salario 
     Usuario:{
         salario(usuario){
@@ -47,6 +64,13 @@ const resolvers = {
                 idade:23,
                 salario_real:1234.56,
                 vip: true
+            }
+        },
+        produtoEmDestaque(){
+            return{
+                nome:'Notebook Games',
+                preco:'4890.89',
+                desconto:'0.5'
             }
         }
     }
